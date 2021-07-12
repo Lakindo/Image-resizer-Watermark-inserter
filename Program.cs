8f083ed4953735720bf45f1d0771d7aa5890926e
Lazar Govedarević <77217401+Lakindo@users.noreply.github.com>
2021-07-13 00:29:38 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,34 +1,43 @@
 ﻿using System;
+using System.Drawing.Imaging;
 using System.Drawing;
-using System.IO;
 using System.Drawing.Drawing2D;
+using System.IO;
 
-namespace imgWatermark
+namespace ImgResizer
 {
-
-    public class Watermarker : IDisposable
+    class Program
     {
-        public Watermarker(string v)
-        {
-        }
-
-        void IDisposable.Dispose() { }
-
         static void Main(string[] args)
         {
-            using (Watermarker watermarker = new Watermarker("C:\\Users\\Lakindo\\Desktop" +
-                "\\Things\\Pictures\\kris_bald_resized.png"))
-            {
-                using (ImageWatermark watermark = new ImageWatermark ("C:\\Users\\Lakindo\\Desktop\\Things\\" +
-                    "Pictures\\pepelaughpoint.png"))
-                {
-                    watermark.X = 30;
-                    watermark.Y = 50;
 
-                    watermark.Add(watermark);
-                    watermark.Save("C:\\Users\\Lakindo\\Desktop\\Things\\Pictures\\kris_bald_resized.png");
+            Image image = Image.FromStream(File.OpenRead("C:\\Users\\Lakindo\\Desktop\\Things\\Pictures\\kris bald.png"));
+            var destRect = new Rectangle(0, 0, 700, 576);
+            var output = new Bitmap(500, 376);
+
+            output.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var gr = Graphics.FromImage(output))
+            {
+
+                gr.CompositingMode = CompositingMode.SourceCopy;
+                gr.CompositingQuality = CompositingQuality.HighQuality;
+                gr.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                gr.SmoothingMode = SmoothingMode.HighQuality;
+                gr.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+
+                    gr.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
                 }
             }
+
+            image.Dispose();
+            output.Save("C:\\Users\\Lakindo\\Desktop\\Things\\Pictures\\kris bald resized.png");
+
         }
     }
 }
