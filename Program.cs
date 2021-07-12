6fafba594d0e148d9177de7f81ea6575116da2ad
Lazar Govedarević <77217401+Lakindo@users.noreply.github.com>
2021-07-13 00:28:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Drawing2D;
+
+namespace imgWatermark
+{
+
+    public class Watermarker : IDisposable
+    {
+        public Watermarker(string v)
+        {
+        }
+
+        void IDisposable.Dispose() { }
+
+        static void Main(string[] args)
+        {
+            using (Watermarker watermarker = new Watermarker("C:\\Users\\Lakindo\\Desktop" +
+                "\\Things\\Pictures\\kris_bald_resized.png"))
+            {
+                using (ImageWatermark watermark = new ImageWatermark ("C:\\Users\\Lakindo\\Desktop\\Things\\" +
+                    "Pictures\\pepelaughpoint.png"))
+                {
+                    watermark.X = 30;
+                    watermark.Y = 50;
+
+                    watermark.Add(watermark);
+                    watermark.Save("C:\\Users\\Lakindo\\Desktop\\Things\\Pictures\\kris_bald_resized.png");
+                }
+            }
+        }
+    }
+}
